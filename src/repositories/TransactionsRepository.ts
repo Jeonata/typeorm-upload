a178e5c5a5eae8d516d13c48f41881c554aee8e1
Jeonata Oliveira <jeo.nata@gmail.com>
2020-04-23 23:05:32 -0300

    fix: crud transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,21 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const income = transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((sum, transaction) => sum + Number(transaction.value), 0);
+
+    const outcome = transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((sum, transaction) => sum + Number(transaction.value), 0);
+
+    const total: number = income - outcome;
+
+    const balace = { income, outcome, total };
+
+    return balace;
   }
 }
 
